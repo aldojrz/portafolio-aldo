53d78faacd2e2abbd727d717cee04dcc51c5e458
aldojrz <aldojrz2210@gmail.com>
2026-02-18 16:16:47 -0600

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// Efecto typing
-const texto = "Desarrollador Web | Analista de Sistemas | Programador";
-let i = 0;
-
-function typing() {
-    if (i < texto.length) {
-        document.getElementById("typing").innerHTML += texto.charAt(i);
-        i++;
-        setTimeout(typing, 50);
-    }
-}
-
-typing();
-
-function mensaje() {
-    document.getElementById("mensaje").innerHTML =
-    "Gracias por tu interés. Estoy disponible para proyectos tecnológicos.";
-}
+console.log("Portafolio cargado correctamente 🚀");
